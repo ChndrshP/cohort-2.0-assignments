676a2a0421de4d607e72a7ffc19b2f0a562f0add
ChndrshP <chndrsh25@gmail.com>
2024-08-14 18:09:24 +0530

    Mongo with jwt auth solved

---
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://chndrsh25:n1pmPEEwpsmwOz0D@cluster0.kxa9e.mongodb.net/');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    PurchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
